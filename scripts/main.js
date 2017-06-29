cc3cf605b76459bc59df27c6c50934d8ccfb60ce
Chandrashekar S <chandrashekar998@gmail.com>
2017-06-29 22:52:21 +0530

    Starting template for a basic react project

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+ const React = require('react');
+ const ReactDOM = require('react-dom');
+/*
+Store Picker
+*/
+
+var StorePicker = React.createClass({
+  render:function(){
+    return (
+      <h1>hello</h1>
+    )
+  }
+});
+
+ReactDOM.render(<StorePicker/>,document.querySelector('#main'));
