9cea4e0e843bfb0ad659aac209ddadb71f9486bc
Chandrashekar S <chandrashekar998@gmail.com>
2017-06-29 23:23:12 +0530

    Creating elements classes and components

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,56 @@
 /*
 Store Picker
 */
-
-var StorePicker = React.createClass({
+var App=React.createClass({
   render:function(){
-    return (
-      <h1>hello</h1>
+    return(
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
     )
   }
 });
 
-ReactDOM.render(<StorePicker/>,document.querySelector('#main'));
+var Header=React.createClass({
+  render:function(){
+    return(
+      <p>Header</p>
+    )
+  }
+});
+var Order=React.createClass({
+  render:function(){
+    return(
+      <p>Order</p>
+    )
+  }
+});
+var Inventory=React.createClass({
+  render:function(){
+    return(
+      <p>Inventory</p>
+    )
+  }
+});
+//create an class and render through an id
+// or  Create new DOM Element using React.createElement();
+var StorePicker = React.createClass({
+  render:function(){
+    var name="Chandru";
+    return (
+    <form className="store-selector">
+      <h2>Please Enter A Store {name}</h2>
+      <input type="text" ref="storeId" required/>
+      <input type="submit"/>
+    </form>
+    )
+    //Or
+    //return React.createElement('p',{className:'testing'},'Hello World');
+  }
+});
+
+ReactDOM.render(<App/>,document.querySelector('#main'));
